78d0cbcf57b48eb7cef832f2701a595ef6c436c7
Allan Barbosa <115508413+AllanBOMelo@users.noreply.github.com>
2024-12-06 00:43:39 -0300

    Update database.py

--- db/database.py
+++ db/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", f"mysql+pymysql://{user}:{password}@db/test_db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", f"mysql+pymysql://{user}:{password}@db/users")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"host": "db", "port": 3306})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
